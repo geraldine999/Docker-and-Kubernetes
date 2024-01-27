9c8a3e4f3a8d27e22a2e4ffce0650ffcb62b9f64
geraldine999 <gadelmann@agro.uba.ar>
2024-01-27 19:44:01 -0300

    logs

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -1,13 +1,24 @@
 package com.example.hello;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(HelloApplication.class);
+
+	@PostConstruct
+	public static void init(){
+		logger.info("💜 Application loading...");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
+		logger.info("💜 Application executed.");
 	}
 
 }
--- src/test/java/com/example/hello/HelloApplicationTests.java
+++ src/test/java/com/example/hello/HelloApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.hello;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class HelloApplicationTests {
 
+	Logger logger = LoggerFactory.getLogger(HelloApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		logger.info("💜 Executing test...");
+		assertEquals(true, true);
+		logger.info("💜 Test executed");
 	}
 
 }
